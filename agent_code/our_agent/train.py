4a566b72f410bb81a59b1294b44e072f2d82c1e2
Jonas <jonas.hellgoth@gmx.de>
2021-03-18 16:36:17 +0100

    commit to colab

--- agent_code/our_agent/train.py
+++ agent_code/our_agent/train.py
@@ -12,9 +12,9 @@ import torchvision
 
 from torch.utils.tensorboard import SummaryWriter
 
-from callbacks import state_to_features
+from agent_code.our_agent.callbacks import state_to_features
 
-from modified_rule_based_agent import Modified_Rule_Based_Agent
+from agent_code.our_agent.modified_rule_based_agent import Modified_Rule_Based_Agent
 
 
 ACTIONS = ['UP', 'RIGHT', 'DOWN', 'LEFT', 'WAIT', 'BOMB']
