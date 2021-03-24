503fb11808e728d67c751357ba01c796fad31d5b
Jonas <jonas.hellgoth@gmx.de>
2021-03-24 20:19:55 +0100

    remove dropout layer

--- neural_network_pretraining/neural_network.py
+++ neural_network_pretraining/neural_network.py
@@ -56,6 +56,44 @@ class OurNeuralNetwork_conv(nn.Module):
         
         return out
 
+class OurNeuralNetwork_conv_2(nn.Module):
+    def __init__(self):
+        super(OurNeuralNetwork_conv, self).__init__()
+
+        self.conv1 = nn.Conv2d(7, 32, kernel_size=5, padding=1)
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=3)
+        self.conv_dropout = nn.Dropout2d()
+
+        self.linear1 = nn.Linear(64*2*2, 64)
+        self.linear2 = nn.Linear(64, 6)
+
+    def forward(self, x):
+        out = self.conv1(x) # bs x 32 x 15 x 15
+        #print(out.size()) # 15 is not divisible by 2 --> problem
+        out = F.max_pool2d(out, 2) # bs x 32 x 7 x 7
+        #print(out.size())
+        out = F.relu(out) # bs x 32 x 7 x 7
+        #print(out.size())
+
+        out = self.conv2(out) # bs x 64 x 5 x 5
+        #print(out.size())
+        #out = self.conv_dropout(out) # bs x 64 x 5 x 5
+        #print(out.size())
+        out = F.max_pool2d(out, 2) # bs x 64 x 2 x 2
+        #print(out.size())
+        out = F.relu(out) # bs x 64 x 2 x 2
+        #print(out.size())
+        
+        out = out.view(-1, 64*2*2) # bs x 256
+        #print(out.size())
+        out = self.linear1(out) # bs x 64
+        #print(out.size())
+        out = F.relu(out) # bs x 64
+        #print(out.size())
+        out = self.linear2(out) # bs x 6
+        #print(out.size())
+        
+        return out
 '''
 import numpy as np
 model = OurNeuralNetwork_conv()
